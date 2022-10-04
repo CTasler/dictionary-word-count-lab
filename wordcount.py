0d81859a28aa823beb20132eef5ab7fc491c7615
Claire Tasler <tasl0368@gmail.com>
2022-10-03 23:23:44 -0500

    cleaned up comments

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,5 @@
 """Count words in file."""
 
-
-# put your code here.
 from string import punctuation
 import sys
 import string
@@ -15,23 +13,22 @@ def tokenize(file):
         line = line.rstrip().split(" ")
         for word in line: 
             word = word.translate(str.maketrans(" "," ", string.punctuation))
+            #replaces all punctuation in word with None
             word = word.title()
             words.append(word)
     return words
-    # for word in words: 
-    #     word_count[word] = word_count.get(word, 0) + 1
-    # for word, count in word_count.items():
-    #     print(word, count)
+
+
 def count_words(words):
     word_count = {}
     for word in words: 
         word_count[word] = word_count.get(word, 0) + 1
     return word_count
     
+    
 def print_word_count(word_count):
     for word, count in word_count.items():
         print(word, count)
-
 
 
 print_word_count(count_words(tokenize(file)))

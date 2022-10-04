93a0f40343b05ab5497c6cdd665ed1cb1e408329
Claire Tasler <tasl0368@gmail.com>
2022-10-03 23:14:37 -0500

    added tokenize() count_words() and print_word_count()

--- wordcount.py
+++ wordcount.py
@@ -9,20 +9,35 @@ import string
 
 file = open(sys.argv[1])
 
-def print_word_count(file):
+def tokenize(file):
     words = []
-    word_count = {}
-    punctuation_marks = [',', '.', ':', ';', '"', '--', '?']
     for line in file: 
         line = line.rstrip().split(" ")
         for word in line: 
             word = word.translate(str.maketrans(" "," ", string.punctuation))
             word = word.title()
             words.append(word)
+    return words
+    # for word in words: 
+    #     word_count[word] = word_count.get(word, 0) + 1
+    # for word, count in word_count.items():
+    #     print(word, count)
+def count_words(words):
+    word_count = {}
     for word in words: 
         word_count[word] = word_count.get(word, 0) + 1
+    return word_count
+    
+def print_word_count(word_count):
     for word, count in word_count.items():
         print(word, count)
 
 
-print_word_count(file)
+
+print_word_count(count_words(tokenize(file)))
+
+"""Can write above code as below:
+tokens = tokenize(filename)
+word_count = count_words(words)
+print_word_count(word_count)
+"""
